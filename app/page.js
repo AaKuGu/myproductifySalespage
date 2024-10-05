2050ed4b96714f60cb11aec96e21f50106e1afaa
Aadarsh Kumar Gupta <prathamtogupta11@gmail.com>
2024-10-05 17:54:38 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
+import FrontendSalesPage from "@/components/salespage/frontend/FrontendSalesPage";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return <FrontendSalesPage />;
 };
 
 export default page;
