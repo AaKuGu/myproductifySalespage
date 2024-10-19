e6db879b5c2df6540a55c38a6fc475f283176380
Aadarsh Kumar Gupta <prathamtogupta11@gmail.com>
2024-10-20 00:36:42 +0530

    dksjf

--- app/thankYou/page.jsx
+++ app/thankYou/page.jsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
 const ThankYouPage = () => {
   return (
     <div className="flex items-start justify-center min-h-screen p-4 bg-gradient-to-br from-gray-200 to-gray-400">
       <div className="w-full max-w-lg p-6 bg-white rounded-lg shadow-lg flex flex-col items-center">
-        <img
+        <Image
+          width={300}
+          height={300}
           src="logo/me2.png" // Replace with your photo URL
           alt="Aadarsh Gupta"
           className="w-40 h-40 rounded-full border-4 border-indigo-500 mb-4"
