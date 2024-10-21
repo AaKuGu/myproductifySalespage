81b05fb159c08377b451470bdee97466b19ebd24
Aadarsh Kumar Gupta <prathamtogupta11@gmail.com>
2024-10-21 14:05:14 +0530

    h

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -23,6 +23,16 @@ const page = () => {
       <a href="https://warriorplus.com/o2/buy/g886gd/r1wc27/rqhzg3">
         <img src="https://warriorplus.com/o2/btn/fn100011001/g886gd/r1wc27/411410" />
       </a>
+
+      <iframe
+        src="https://warriorplus.com/o2/btn/fn100011001/g886gd/zjd7fx/411403"
+        frameBorder="0"
+        width="100%"
+        height="50" // Adjust height based on button size
+        scrolling="no"
+        className="rounded-lg shadow-lg"
+        title="WarriorPlus Buy Button"
+      ></iframe>
     </div>
   );
 };
