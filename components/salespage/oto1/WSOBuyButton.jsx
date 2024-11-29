fb908fc1d1402e33ab1b4c6f838dd449354c3a94
Aadarsh Kumar Gupta <prathamtogupta11@gmail.com>
2024-11-29 21:52:43 +0530

    buy button on bundle upsell page

--- components/salespage/oto1/WSOBuyButton.jsx
+++ components/salespage/oto1/WSOBuyButton.jsx
@@ -7,15 +7,19 @@ const WSOUpsellBuyButton = () => {
     <Link
       href={"https://warriorplus.com/o2/buy/g886gd/r1wc27/rqhzg3"}
       target="_blank"
-      className={`${""} w-full rounded-lg h-auto text-black flex items-center justify-center`}
+      className={`${""} w-auto rounded-lg h-auto text-black flex items-center justify-center `}
       id="WSOUpsellBuyButton"
     >
       <Image
-        src={"/logo/bundleUpsellBuyButton.png"}
+        src={"/buyButtons/bundleUpsell.png"}
         width={500}
         height={500}
         className={`${""} w-full lg:w-[40vw]`}
       />
+      <img
+        src="https://warriorplus.com/o2/btn/fn100011001/g886gd/r1wc27/411410"
+        className={`${""} hidden`}
+      ></img>
     </Link>
   );
 };
